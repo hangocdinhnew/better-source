7aa51f2e44a3766d972752e71e2a79a9d41cd57a
hangocdinhnew <dinhndrwnew@gmail.com>
2025-09-23 20:21:31 +0700

    Nix file.

---
+++ Libraries/default.nix
@@ -0,0 +1,47 @@
+{
+  pkgs ? import <nixpkgs> { },
+}:
+with pkgs;
+
+let
+    buildDeps = with pkgs; [
+        clang-tools
+        cmake
+        pkg-config
+        ninja
+        cppcheck
+        doxygen
+        makeWrapper
+    ];
+
+    runtimeDeps = with pkgs; [
+        SDL2
+        freetype
+        fontconfig
+        openal
+        libjpeg
+        curl
+        bzip2
+        libedit
+    ];
+
+    libraryPath = pkgs.lib.makeLibraryPath runtimeDeps;
+in
+mkShell.override { } {
+    nativeBuildInputs = with pkgs; [
+    ] ++ buildDeps;
+
+    buildInputs = with pkgs; [
+    ] ++ runtimeDeps;
+
+    inputsFrom = with pkgs; [
+    ] ++ runtimeDeps;
+
+    shellHook = ''
+        if [ "$(uname)" = "Darwin" ]; then
+            export DYLD_LIBRARY_PATH=${libraryPath}:$DYLD_LIBRARY_PATH
+        else
+            export LD_LIBRARY_PATH=${libraryPath}:$LD_LIBRARY_PATH
+        fi
+    '';
+}
